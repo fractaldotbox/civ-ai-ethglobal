d9e2d0cfed5c36af2007e8ddacb4a5b79c8e5c15
ken <kenyiu@users.noreply.github.com>
2024-06-03 19:17:00 +0800

    fix: env name for nextjs to recognize the weatherxm domain correctly

--- packages/engine/src/domain/weather.ts
+++ packages/engine/src/domain/weather.ts
@@ -1,5 +1,6 @@
 export const getWeather = (cellId: string = '876526addffffff') => {
-  const domain = process.env.WEATHERXM_DOMAIN || 'https://api.weatherxm.com/api/v1';
+  const domain = process.env.NEXT_PUBLIC_WEATHERXM_DOMAIN || 'https://api.weatherxm.com/api/v1';
+  console.log('the weatherxm domain is ', domain);
   const endpoint = `${domain}/cells/${cellId}/devices`
   return fetch(endpoint).then((res) =>
     res.json(),
