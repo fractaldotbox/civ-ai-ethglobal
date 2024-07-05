b8847dcba99aef1c62f8b30d68a4e0c93d81a1c9
debuggingfuture <lauchunyin@debuggingfuture.com>
2024-07-05 16:21:57 +0800

    fix: asset path

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   basePath: "/civ-ai-ethglobal",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/civ-ai-ethglobal" : undefined,
+  // assetPrefix: process.env.NODE_ENV === "production" ? "/civ-ai-ethglobal" : undefined,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
